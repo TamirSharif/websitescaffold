856925e956f34bd1344cbc46b5052baeecd866d0
Tamir Sharif <tamir.sharif@gmail.com>
2021-10-18 10:57:16 +0100

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from "./pages/LoginPage";
